7c183a58bb947b2dba8596ed9e796b2733a80e50
Tzikas <niko.tzikas@gmail.com>
2017-09-06 16:12:02 -0400

    sass and jade

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var USERS = require('../users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.body);
-  res.render('index', { title: 'dog' });
+  res.render('index', { title: 'Express' });
 });
-router.get('/test', function(req, res, next) {
-  res.render('index', { title: 'test' });
-});
-router.get('/all', function(req, res, next) {
-  console.log(USERS);
-  res.render('all', { users: USERS });
 
-});
 module.exports = router;
