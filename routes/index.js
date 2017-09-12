8f0394d82ea4707d97f0d1156d99ca0abbe35f90
paulinero91 <paulinero91@gmail.com>
2017-09-12 15:40:52 -0400

    first commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'asdf' });
 });
 
+router.get('/canvas', function(req, res, next) {
+  res.render('canvas', { title: 'Start a New Project' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Add to a Project' });
+});
+
+
 module.exports = router;
