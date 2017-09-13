d98a98caae8909f69863c3a76e2c0e5b89b0b68f
Tzikas <niko.tzikas@gmail.com>
2017-09-13 13:21:44 -0400

    wokring routers

--- single/router.js
+++ single/router.js
@@ -10,10 +10,45 @@ const jsonParser = bodyParser.json();
 
 router.post('/', function(req, res, next) {
 	console.log('hi')
-  	Canvas.create({simple: 'yoyo'})
+  	//Canvas.create({simple: 'yoyo'}, function(a,b){ console.log(a,b) })
+	var obj = {simple: 'yoyo'}; 
+	let id = ""; 
+  	Canvas.create(obj, function(err, yo){ 
+		
+		console.log(err)
+	       if (err) return; 
+		console.log('sup')
+		console.log(yo._id)
+		id = yo._id; 
 	
-	res.render('canvas', { title: 'new' });
+
+		res.redirect(`new/canvas/${id}`)
+
+	})
+		
 });
+
+
+router.get('/canvas/:id', function(req, res, next) {
+	console.log('in heresss') 
+	console.log(req.params.id);
+	Canvas.find({'_id':req.params.id}, function(err, r){
+		console.log('u')
+		console.log(r);
+		res.render('canvas', { canvas: r[0].canvas});
+	
+	})
+
+
+})
+
+/*
+collection.insert(objectToInsert, function(err){
+   if (err) return;
+   // Object inserted successfully.
+   var objectId = objectToInsert._id; // this will return the id of object inserted
+});
+*/
 
 
 
